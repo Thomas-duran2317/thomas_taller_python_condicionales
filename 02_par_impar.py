0b7c82208fabb7978f5137e3033f23b5cc2beea8
Thomas Duran <tduran@estud.usfq.edu.ec>
2026-02-26 08:10:06 -0500

    Segundo ejercicio python del taller

---
+++ 02_par_impar.py
@@ -0,0 +1,11 @@
+num = int(input("Ingrese un número entero cualquiera"))
+if num%2 == 0:
+	print ("El numero es par")
+else:
+	print ("El numero es impar")
+
+edad = int(input("Ingrese su edad"))
+if edad>=18: 
+	print ("Eres mayor de edad")
+else:
+	print("Eres menor de edad")
